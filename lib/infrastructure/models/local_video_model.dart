212313a0d341be00c36ec542247b1a44cb80a18d
josh_MacBook <josuearroliga89@gmail.com>
2024-05-03 23:01:25 -0600

    End of day commit

---
+++ lib/infrastructure/models/local_video_model.dart
@@ -0,0 +1,29 @@
+import 'package:toktik/domain/entities/video_post.dart';
+
+class LocalVideoModel {
+  String name;
+  String videoUrl;
+  int likes;
+  int views;
+
+  LocalVideoModel(
+      {required this.name,
+      required this.videoUrl,
+      this.likes = 0,
+      this.views = 0});
+
+  factory LocalVideoModel.fromJson(Map<String, dynamic> json) =>
+      LocalVideoModel(
+        name: json['name'],
+        videoUrl: json['videoUrl'],
+        likes: json['likes'],
+        views: json['views'],
+      );
+
+  VideoPost toVideoPostEntity() => VideoPost(
+        caption: name,
+        videoUrl: videoUrl,
+        likes: likes,
+        views: views,
+      );
+}
