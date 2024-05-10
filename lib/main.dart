2ae620b69e3ba6f66f0907953e1ac7969da470ea
josh_MacBook <josuearroliga89@gmail.com>
2024-05-10 09:05:28 -0600

    Commit may 10th - clean arquitechture

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:toktik/config/app_theme.dart';
+import 'package:toktik/infrastructure/datasources/local_videos_data_source_impl.dart';
+import 'package:toktik/infrastructure/repositories/video_post_repository_impl.dart';
 import 'package:toktik/presentation/providers/discover_provider.dart';
 import 'package:toktik/presentation/screens/discover/discover_screen.dart';
 
@@ -14,11 +16,15 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    final videoPostRepository =
+        VideoPostsRepositoryImpl(videosDataSource: LocalVideoDataSource());
+
     return MultiProvider(
       providers: [
         ChangeNotifierProvider(
           lazy: false,
-          create: (_) => DiscoverProvider()..loadNextPage(),
+          create: (_) => DiscoverProvider(videosRepository: videoPostRepository)
+            ..loadNextPage(),
         ),
       ],
       child: MaterialApp(
